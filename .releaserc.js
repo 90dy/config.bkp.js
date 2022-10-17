e3f404c1ca90a26a00f86f756bf4300d6d87b479
90dy <90dy@proton.me>
2022-10-17 16:21:02 +0200

    chore(ci): setup
    Signed-off-by: 90dy <90dy@proton.me>

--- .releaserc.js
+++ .releaserc.js
@@ -1,3 +1,4 @@
 module.exports = {
 	extends: ['./semantic-config'],
+	branches: ['90dy'],
 }
---
+++ semantic-config.js
@@ -0,0 +1,10 @@
+/** @typedef { import("semantic-release").Config } SemanticReleaseConfig */
+
+/** @type {SemanticReleaseConfig} */
+module.exports = {
+	branches: ['master', 'main'],
+	plugins: [
+		'@semantic-release/commit-analyzer',
+		'@semantic-release/release-notes-generator',
+	],
+}
