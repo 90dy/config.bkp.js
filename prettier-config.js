7822f6e1548b35eaa3055284d320fc9a353a102c
90dy <90dy@proton.me>
2022-10-17 15:48:10 +0200

    chore: js config
    Signed-off-by: 90dy <90dy@proton.me>

---
+++ prettier-config.js
@@ -0,0 +1,9 @@
+/** @typedef { import('prettier').Config } PrettierConfig  */
+
+/** @type {PrettierConfig} */
+export default {
+	printWidth: 80,
+	semi: false,
+	singleQuote: true,
+	jsxSingleQuote: true,
+};
